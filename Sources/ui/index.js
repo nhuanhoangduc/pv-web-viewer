ea769de5eac8e48e67cb96ff4c90403b266f2b3f
Forrest Li <forrest.li@kitware.com>
2018-04-11 16:42:51 -0400

    fix(UI): Add a simple Table component

--- Sources/ui/index.js
+++ Sources/ui/index.js
@@ -1,9 +1,11 @@
 import Button from './Button';
 import FaIcon from './FaIcon';
 import Menu from './Menu';
+import Table from './Table';
 
 export default {
   Button,
   FaIcon,
   Menu,
+  Table,
 };
